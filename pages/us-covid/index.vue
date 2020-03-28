4d1471abd076678de599c997a13ecae3e862fc49
msavast1 <msavast1@its.jnj.com>
2020-03-27 22:50:22 -0400

    fix data api change bug

--- pages/us-covid/index.vue
+++ pages/us-covid/index.vue
@@ -42,19 +42,89 @@
 
 <script>
 import axios from 'axios'
+import { groupBy, remove, sumBy } from 'lodash'
 export default {
   async asyncData(context) {
     const coun = await axios.get('https://api.covid19api.com/countries')
 
-    const con = await axios.get(
-      `https://api.covid19api.com/dayone/country/us/status/confirmed`
+    const conOld = await axios.get(
+      'https://api.covid19api.com/dayone/country/us/status/confirmed'
     )
 
-    const dea = await axios.get(
-      `https://api.covid19api.com/dayone/country/us/status/deaths`
+    const deaOld = await axios.get(
+      'https://api.covid19api.com/dayone/country/us/status/deaths'
     )
 
-    const full = con.data.concat(dea.data)
+    const deaNew = remove(deaOld.data, (el) => {
+      return el.Date > '2020-03-22T00:00:00Z'
+    })
+
+    const conNew = remove(conOld.data, (el) => {
+      return el.Date > '2020-03-22T00:00:00Z'
+    })
+
+    const gCon = conNew.map((el) => {
+      const regex = /^([^,])+/g
+      el.Province = el.Province.match(regex)[0]
+      return el
+    })
+
+    const dCon = deaNew.map((el) => {
+      const regex = /^([^,])+/g
+      el.Province = el.Province.match(regex)[0]
+      return el
+    })
+
+    const dateDea = groupBy(dCon, 'Province')
+
+    const dateCon = groupBy(gCon, 'Province')
+
+    const stateCon = Object.keys(dateCon).map((key) => {
+      return groupBy(dateCon[key], 'Date')
+    })
+
+    const stateDea = Object.keys(dateDea).map((key) => {
+      return groupBy(dateDea[key], 'Date')
+    })
+
+    const aggCon = stateCon
+      .map((state) => {
+        let cs
+        return Object.keys(state).map((date, i) => {
+          cs = sumBy(state[date], (o) => {
+            return o.Cases
+          })
+          return {
+            Province: state[date][0].Province,
+            Status: 'confirmed',
+            Date: state[date][0].Date,
+            Cases: cs
+          }
+        })
+      })
+      .flat()
+
+    const aggDea = stateDea
+      .map((state) => {
+        let cs
+        return Object.keys(state).map((date, i) => {
+          cs = sumBy(state[date], (o) => {
+            return o.Cases
+          })
+          return {
+            Province: state[date][0].Province,
+            Status: 'deaths',
+            Date: state[date][0].Date,
+            Cases: cs
+          }
+        })
+      })
+      .flat()
+
+    const full = aggCon
+      .concat(conOld.data)
+      .concat(deaOld.data)
+      .concat(aggDea)
 
     return {
       full,

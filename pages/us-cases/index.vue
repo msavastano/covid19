bccfc2c59b4bb87a472031886e3904bc884d73d3
msavast1 <msavast1@its.jnj.com>
2020-07-10 23:24:19 -0400

    add full us

--- pages/us-cases/index.vue
+++ pages/us-cases/index.vue
@@ -73,6 +73,7 @@
 <script>
 import axios from 'axios'
 import { groupBy, sumBy } from 'lodash'
+import states from '../../utils/states'
 import LineChart from '@/components/LineChart.vue'
 
 export default {
@@ -137,258 +138,7 @@ export default {
         }
       },
       datacollection: { labels: [], datasets: [] },
-      states: [
-        {
-          name: 'Alabama',
-          pop: 4903185,
-          abbr: 'AL'
-        },
-        {
-          name: 'Alaska',
-          pop: 731545,
-          abbr: 'AK'
-        },
-        {
-          name: 'Arizona',
-          pop: 7278717,
-          abbr: 'AZ'
-        },
-        {
-          name: 'Arkansas',
-          pop: 3017825,
-          abbr: 'AR'
-        },
-        {
-          name: 'California',
-          pop: 39512223,
-          abbr: 'CA'
-        },
-        {
-          name: 'Colorado',
-          pop: 5758736,
-          abbr: 'CO'
-        },
-        {
-          name: 'Connecticut',
-          pop: 3565287,
-          abbr: 'CT'
-        },
-        {
-          name: 'Delaware',
-          pop: 973764,
-          abbr: 'DE'
-        },
-        {
-          name: 'District of Columbia',
-          pop: 705749,
-          abbr: 'DC'
-        },
-        {
-          name: 'Florida',
-          pop: 21477737,
-          abbr: 'FL'
-        },
-        {
-          name: 'Georgia',
-          pop: 10617423,
-          abbr: 'GA'
-        },
-        {
-          name: 'Hawaii',
-          pop: 1415872,
-          abbr: 'HI'
-        },
-        {
-          name: 'Idaho',
-          pop: 1787147,
-          abbr: 'ID'
-        },
-        {
-          name: 'Illinois',
-          pop: 12671821,
-          abbr: 'IL'
-        },
-        {
-          name: 'Indiana',
-          pop: 6732219,
-          abbr: 'IN'
-        },
-        {
-          name: 'Kansas',
-          pop: 2913314,
-          abbr: 'KS'
-        },
-        {
-          name: 'Kentucky',
-          pop: 4467673,
-          abbr: 'KY'
-        },
-        {
-          name: 'Louisiana',
-          pop: 4648794,
-          abbr: 'LA'
-        },
-        {
-          name: 'Maine',
-          pop: 1344212,
-          abbr: 'ME'
-        },
-        {
-          name: 'Maryland',
-          pop: 6045680,
-          abbr: 'MD'
-        },
-        {
-          name: 'Massachusetts',
-          pop: 6949503,
-          abbr: 'MA'
-        },
-        {
-          name: 'Michigan',
-          pop: 9986857,
-          abbr: 'MI'
-        },
-        {
-          name: 'Minnesota',
-          pop: 5639632,
-          abbr: 'MN'
-        },
-        {
-          name: 'Mississippi',
-          pop: 2976149,
-          abbr: 'MS'
-        },
-        {
-          name: 'Missouri',
-          pop: 6137428,
-          abbr: 'MO'
-        },
-        {
-          name: 'Montana',
-          pop: 1068778,
-          abbr: 'MT'
-        },
-        {
-          name: 'Nebraska',
-          pop: 1934408,
-          abbr: 'NE'
-        },
-        {
-          name: 'Nevada',
-          pop: 3080156,
-          abbr: 'NV'
-        },
-        {
-          name: 'New Hampshire',
-          pop: 1359711,
-          abbr: 'NH'
-        },
-        {
-          name: 'New Jersey',
-          pop: 8882190,
-          abbr: 'NJ'
-        },
-        {
-          name: 'New Mexico',
-          pop: 2096829,
-          abbr: 'NM'
-        },
-        {
-          name: 'New York',
-          pop: 19453561,
-          abbr: 'NY'
-        },
-        {
-          name: 'North Carolina',
-          pop: 10488084,
-          abbr: 'NC'
-        },
-        {
-          name: 'North Dakota',
-          pop: 762062,
-          abbr: 'ND'
-        },
-        {
-          name: 'Ohio',
-          pop: 11689100,
-          abbr: 'OH'
-        },
-        {
-          name: 'Oklahoma',
-          pop: 3956971,
-          abbr: 'OK'
-        },
-        {
-          name: 'Oregon',
-          pop: 4217737,
-          abbr: 'OR'
-        },
-        {
-          name: 'Pennsylvania',
-          pop: 12801989,
-          abbr: 'PA'
-        },
-        {
-          name: 'Rhode Island',
-          pop: 1059361,
-          abbr: 'RI'
-        },
-        {
-          name: 'South Carolina',
-          pop: 5148714,
-          abbr: 'SC'
-        },
-        {
-          name: 'South Dakota',
-          pop: 884659,
-          abbr: 'SD'
-        },
-        {
-          name: 'Tennessee',
-          pop: 6833174,
-          abbr: 'TN'
-        },
-        {
-          name: 'Texas',
-          pop: 28995881,
-          abbr: 'TX'
-        },
-        {
-          name: 'Utah',
-          pop: 3205958,
-          abbr: 'UT'
-        },
-        {
-          name: 'Vermont',
-          pop: 623989,
-          abbr: 'VT'
-        },
-        {
-          name: 'Virginia',
-          pop: 8535519,
-          abbr: 'VA'
-        },
-        {
-          name: 'Washington',
-          pop: 7614893,
-          abbr: 'WA'
-        },
-        {
-          name: 'West Virginia',
-          pop: 1792065,
-          abbr: 'WV'
-        },
-        {
-          name: 'Wisconsin',
-          pop: 5822434,
-          abbr: 'WI'
-        },
-        {
-          name: 'Wyoming',
-          pop: 578759,
-          abbr: 'WY'
-        }
-      ],
+      states,
       state: 'California',
       stateTwo: 'Texas',
       stateThree: 'South Carolina',
@@ -537,7 +287,7 @@ export default {
           {
             fillOpacity: 1,
             backgroundColor: `rgba(${this.colors[0][0]}, ${this.colors[0][1]}, ${this.colors[0][2]}, 1)`,
-            label: `${this.state} / confirmed`,
+            label: `${this.state} / confirmed - new`,
             fill: false,
             data: casesArrOne.casesArr
           },
@@ -545,28 +295,28 @@ export default {
             fillOpacity: 1,
             fill: false,
             backgroundColor: `rgba(${this.colors[2][0]}, ${this.colors[2][1]}, ${this.colors[2][2]}, 1)`,
-            label: `${this.stateTwo} / confirmed`,
+            label: `${this.stateTwo} / confirmed - new`,
             data: casesArrTwo.casesArr
           },
           {
             fillOpacity: 1,
             fill: false,
             backgroundColor: `rgba(${this.colors[4][0]}, ${this.colors[4][1]}, ${this.colors[4][2]}, 1)`,
-            label: `${this.stateThree} / confirmed`,
+            label: `${this.stateThree} / confirmed - new`,
             data: casesArrThree.casesArr
           },
           {
             fillOpacity: 1,
             fill: false,
             backgroundColor: `rgba(${this.colors[6][0]}, ${this.colors[6][1]}, ${this.colors[6][2]}, 1)`,
-            label: `${this.stateFour} / confirmed`,
+            label: `${this.stateFour} / confirmed - new`,
             data: casesArrFour.casesArr
           },
           {
             fillOpacity: 1,
             fill: false,
             backgroundColor: `rgba(${this.colors[8][0]}, ${this.colors[8][1]}, ${this.colors[8][2]}, 1)`,
-            label: `${this.stateFive} / confirmed`,
+            label: `${this.stateFive} / confirmed - new`,
             data: casesArrFive.casesArr
           }
         ]
